d11907e392f7178c06bae87a284675834930f0c8
Bradley Hagen <Brad.hagen94@gmail.com>
2023-01-26 13:51:05 -0800

    rollbar added

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '48e3921052b44b398ad1fadadae5ae98',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.listen(4000, () => {
   console.log("server running on port 4000");
 });
