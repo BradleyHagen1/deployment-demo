fba3d4bad6702b997271f3a44d56fafb570bb939
Bradley Hagen <Brad.hagen94@gmail.com>
2023-01-26 14:34:38 -0800

    rollbar added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-app.use = express();
-app.use = cors();
+const app = express();
+app.use(express.json())
+app.use(cors());
 
 app.use(express.static(`${__dirname}/public`))
 
